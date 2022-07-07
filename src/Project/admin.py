83d7525e29c71cb058ca1d80dcc22fbf23a2124d
Daniel Penado <penadodan02@gmail.com>
2022-07-07 17:37:45 -0600

    Advances on the about page

--- src/Project/admin.py
+++ src/Project/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Project, Technologies, Programming_Language, Framework, Tool, Operating_System, Project_Images
 
 # Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Technologies)
+class TechnologyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Programming_Language)
+class ProgrammingLangAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Framework)
+class FrameworkAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tool)
+class ToolAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Operating_System)
+class OSAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Project_Images)
+class ProjectImgAdmin(admin.ModelAdmin):
+    pass
